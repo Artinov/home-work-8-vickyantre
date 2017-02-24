ca576a05f625341d08c3bb64a096280667fdfb8a
vicky.antre <vicky.antre@gmail.com>
2017-02-24 14:52:27 +0200

    finished homework

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ function setText(selector, text) {
 	*/
 
 	// Write your code here
-}
+	var element = document.querySelector(selector);
+	element.innerText = text;
+};
+
+
 
 
 function setTextAll(selector, text) {
 	// Write your code here
+	var arrayElements = document.querySelectorAll(selector);
+	arrayElements.forEach(function(element){
+	element.innerText = text;
+});
+
 }
 
 
-setText("p", "wasn't that hard, yeah ?");
+
 setTextAll("div .block a", "or was it ?");
+setText("p", "wasn't that hard, yeah ?");
+setText("p", "What`s up?");
+
